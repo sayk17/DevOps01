0894b0835d4db80bb78f8f605072972b1da8f2fc
Ubuntu <ubuntu@ip-172-31-5-17.us-west-1.compute.internal>
2019-10-29 03:46:09 +0000

    Add devOps01a.sh to the repo

--- devops01a.sh
+++ devops01a.sh
@@ -1,13 +1,13 @@
-echo "--------------------------------------------------------------"
+echo "------------------------- VOLUMES DETAILS ----------------------------------------"
 echo "Number of Volumes: `eval df -h | sed -n 1!p | wc -l`"
 echo "Volume Details: "
 df -h --output=source,size,avail
-echo "--------------------------------------------------------------"
+echo "-------------------------- CPU DETAILS ------------------------------------"
 echo "Number of CPUs/Core: `eval nproc`"
 echo "CPU Details: "
 cat /proc/cpuinfo
-echo "---------------------------------------------------------------"
+echo "--------------------------- RAM ------------------------------------"
 echo "Amount of RAM : `eval cat /proc/meminfo | awk '/MemTotal/ {print $2 $3}'`"
-echo "---------------------------------------------------------------"
+echo "--------------------------- NETWORK DETAILS ------------------------------------"
 echo "Mac Address: `eval ifconfig eth0 | awk '/Link encap/ {print $5}'`"
 echo "IP Address: `eval ifconfig eth0 | awk '/inet addr/ {print $2}' | sed 's/addr://g'`"
